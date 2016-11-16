ca39b9f065143322d4133b864ae50b6d90af3714
Eugene Marchenko <3.marchenko@gmail.com>
2016-11-16 17:11:51 +0300

    new branch

--- modules/logrotate/features/steps/cron.rb
+++
@@ -1,6 +0,0 @@
-Then /^logrotate job "([^\"]*)" should be "([^\"]*)"$/ do  |name, state|
-  steps %Q{
-    Then there should be a resource "Logrotate[#{name}]"
-    And the state should be "#{state}"
-  }
-end
--- modules/ruby/features/support/world.rb
+++
@@ -1,9 +0,0 @@
-
-require 'cucumber-puppet/puppet'
-require 'cucumber-puppet/steps'
-
-World do
-  CucumberPuppet.new
-end
-World(CucumberPuppet::Helpers)
-World(CucumberPuppet::Matchers)
