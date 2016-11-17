4a90019693a981f4cb66d515f83ad584e21040ca
Eugene Marchenko <3.marchenko@gmail.com>
2016-11-18 00:16:06 +0300

    test new fact

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
-  $roless = split($facts['roles'], '\s*,\s*')
+  # $roless = split($facts['roles'], '\s*,\s*')
   notify { 'test message': message => "Custom fact is ${facts['roles']} message", }
-  if 'developer' in $roless {
+  if 'developer' in $facts['roles'] {
     notify { 'inside if statement ' : message => "Developer in roles" }
   }
-  if 'run' in $roless {
+  if 'run' in $facts['roles'] {
     notify { 'inside statement 2' : message => "Test in roles" }
   }
 }
