a4da0d56026a943c270727b28fdc0fb2840ca125
Eugene Marchenko <3.marchenko@gmail.com>
2016-11-17 23:01:37 +0300

    test new fact

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 notify { 'test message': message => "Custom fact is ${facts['roles']} message", }
 if 'developer' in $facts['roles'] {
-  notify { 'inside if statement ': message => "Developer in roles"}
+  notify { 'inside if statement ': message => "Developer in roles" }
 }
