a81619cc0b69c4a8ac98c278b42f7c03bad321ad
Eugene Marchenko <3.marchenko@gmail.com>
2016-11-17 23:48:49 +0300

    test new fact

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,4 @@
+$roles = split($::roles, '\s*,\s*')
 notify { 'test message': message => "Custom fact is ${facts['roles']} message", }
 if 'developer' in $facts['roles'] {
   notify { 'inside if statement ' : message => "Developer in roles" }
