097e9892e1dc40c53bc14af2c1f0b5c78894897c
Eugene Marchenko <3.marchenko@gmail.com>
2016-12-14 15:26:33 +0300

    reconfigure

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,5 @@
 node default {
-  # $roless = split($facts['roles'], '\s*,\s*')
-  notify { 'test message' : message => "Custom fact is ${facts['roles']} message", }
-  if 'developer' in $facts['roles'] {
-    notify { 'inside if statement ' : message => "Developer in roles", }
-  }
-  if 'run' in $facts['roles'] {
-    notify { 'inside statement 2' : message => "Test in roles", }
-  }
 
-  each($facts['system_uptime']) |$type, $value| {
-    notice("system has been up ${value} ${type}")
-  }
+  include roles::base
+
 }
---
+++ site/roles/base.pp
@@ -0,0 +1,14 @@
+class roles::base() {
+  notify { 'test message' : message => "Custom fact is ${facts['roles']} message", }
+  if 'developer' in $facts['roles'] {
+    notify { 'inside if statement ' : message => "Developer in roles", }
+  }
+  if 'run' in $facts['roles'] {
+    notify { 'inside statement 2' : message => "Test in roles", }
+  }
+
+  each($facts['system_uptime']) |$type, $value| {
+  notice("system has been up ${value} ${type}")
+}
+
+}
