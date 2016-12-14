06fb5d7eb019aaa74665cf38c3ce41a9c312a596
Eugene Marchenko <3.marchenko@gmail.com>
2016-12-14 15:35:17 +0300

    reconfigure

--- base.pp
+++ base.pp
@@ -1,4 +1,4 @@
-class ::base() {
+class base() {
   notify { 'test message' : message => "Custom fact is ${facts['roles']} message", }
   if 'developer' in $facts['roles'] {
     notify { 'inside if statement ' : message => "Developer in roles", }
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 node default {
 
-  include ::base
+  include base
 
 }
