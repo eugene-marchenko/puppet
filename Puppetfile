7b6b731575571623d91a0754d1f4bf82576b21ba
Eugene Marchenko <3.marchenko@gmail.com>
2016-12-14 15:59:51 +0300

    reconfigure

--- Puppetfile
+++ Puppetfile
@@ -1,2 +1,2 @@
 mod 'puppetlabs-stdlib', '4.13.1'
-mod 'willdurand/nodejs', '2.x.x'
+mod 'willdurand-nodejs', '2.0.0-alpha1'
--- manifests/nodeserver.pp
+++ manifests/nodeserver.pp
@@ -1,6 +1,6 @@
 class nodeserver() {
   notify { 'inside statement 2' : message => "Test in roles", }
   class { 'nodejs':
-    version => 'v7.2.1',
+    version => 'latest',
   }
 }
