c6c665478c42b2f1d07daa38ecd60b8081da6fd2
Eugene Marchenko <3.marchenko@gmail.com>
2016-12-14 15:52:07 +0300

    reconfigure

--- manifests/nodeserver.pp
+++ manifests/nodeserver.pp
@@ -1,3 +1,8 @@
 class nodeserver() {
   notify { 'inside statement 2' : message => "Test in roles", }
+  class { '::nodejs':
+    nodejs_dev_package_ensure => 'present',
+    npm_package_ensure        => 'present',
+    repo_class                => '::epel',
+  }
 }
